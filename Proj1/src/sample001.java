938e80f960b0ade6e6603e533a8a2e704844dbd5
tkd48 <tkd5341@gmail.com>
2022-05-16 13:53:34 +0900

    初期作成

---
+++ Proj1/src/sample001.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ntakeda
+ *
+ */
+public class sample001 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello,World!!");
+	}
+
+}
